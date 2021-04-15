675185bbea9b64d5630e8f48234e46758e15b5d5
tuckner <tuckner329@gmail.com>
2021-04-14 21:50:11 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "tines_token" {
+    type = string
+}
+
+variable "tines_email" {
+    type = string
+}
+
+variable "tines_base_url" {
+    type = string
+}
+
+variable "team_id" {
+    type = number
+    default = "1"
+}
